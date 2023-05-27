afef3479e749d017c023c3889f7589083b93b526
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-27 15:21:19 -0700

    order validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,9 @@ class Order < ApplicationRecord
     has_many :customizations, through: :products
 
     belongs_to :customer
+
+    validates :total, numericality: { greater_than: 0 }
+    validates :discount, numericality: { lass_than: 1 }
+    validates :shipping, presence: true
+    validates :status, presence: true
 end
