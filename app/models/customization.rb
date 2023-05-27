fcc3d07f59b0ed9d5a1de0ce155dec19f8b5ec18
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-27 15:23:18 -0700

    customization validations

--- app/models/customization.rb
+++ app/models/customization.rb
@@ -1,4 +1,8 @@
 class Customization < ApplicationRecord
     has_many :products
     has_many :orders, through: :products
+
+    validates :custom_type, presence: true, inclusion: { in: %w(phrase word date) }
+    validates :price, numericality: { greater_than: 0 }, inclusion: { in: %w(2.00 4.00) }
+    validates :personalization, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ApplicationRecord
     belongs_to :customization
 
     validates :jewelry, presence: true, inclusion: { in: %w(necklace bracelet keychain) }
-    validates :price, numericality: { greater_than: 0 }
+    validates :price, numericality: { greater_than: 0 }, inclusion: { in: %w(8.00 9.00 10.00) }
     validates :quantity, numericality: { greater_than: 0 }
 end
