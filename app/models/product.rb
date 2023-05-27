a007aea722c2cfd1d847b7474a2b429a759cbd20
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-27 15:19:57 -0700

    product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
     belongs_to :order
     belongs_to :customization
+
+    validates :jewelry, presence: true, inclusion: { in: %w(necklace bracelet keychain) }
+    validates :price, numericality: { greater_than: 0 }
+    validates :quantity, numericality: { greater_than: 0 }
 end
