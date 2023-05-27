a43bc1b39bc5d46e0020f31f69b25ee40c1dac79
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-27 16:19:47 -0700

    index and show added to customizaion

--- app/controllers/customizations_controller.rb
+++ app/controllers/customizations_controller.rb
@@ -1,2 +1,10 @@
 class CustomizationsController < ApplicationController
+
+    def index 
+        render json: Customization.all, status: :ok
+    end
+
+    def show
+        render json: @customization, status: :ok
+    end
 end
