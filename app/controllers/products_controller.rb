cf65305ec74d7a59f1cc3dfc1659a800e4492f54
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-27 16:17:45 -0700

    show method added to product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,8 @@ class ProductsController < ApplicationController
     def index 
         render json: Product.all, status: :ok
     end
+
+    def show
+        render json: @product, status: :ok
+    end
 end
