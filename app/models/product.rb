109833f1b0e7be65cdd78582f8e7bd4fe7f0ca60
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-27 15:36:44 -0700

    custom validation added to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,12 @@ class Product < ApplicationRecord
     validates :jewelry, presence: true, inclusion: { in: %w(necklace bracelet keychain) }
     validates :price, numericality: { greater_than: 0 }, inclusion: { in: %w(8.00 9.00 10.00) }
     validates :quantity, numericality: { greater_than: 0 }
+    validate :within_24_hours
+
+    private
+
+    def within_24_hours
+        errors.add(:created_at, "must be within 24 hours of order") unless order.created_at > Time.now-1.day
+    end
+
 end
