23722b36e6961bd4d1c35c51674af79c1736a543
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-27 15:24:00 -0700

    customer validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,9 @@
 class Customer < ApplicationRecord
     has_many :orders
 
+    validates :name, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+
     has_secure_password
 end
