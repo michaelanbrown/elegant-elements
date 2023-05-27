698fa95670b43848c08f78834df5dceb986edbf8
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-27 16:12:34 -0700

    sessions controller built out and authenticate_customer method created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_customer
+
   include ActionController::Cookies
+
+  def current_customer
+    @current_customer ||= Customer.find_by(id: session[:customer_id])
+  end
+
+  private
+
+  def authenticate_customer
+    render json: { errors: "Not authorized" }, status: :unauthorized unless current_customer
+  end
 
 end
