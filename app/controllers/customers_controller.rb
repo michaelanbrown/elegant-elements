9ab8c3e185c4df972fb892f769323538670fb23b
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-27 15:58:49 -0700

    added create method to customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,5 +3,16 @@ class CustomersController < ApplicationController
     def index 
         render json: Customer.all, status: :ok
     end
-    
+
+    def create
+        customer = Customer.create!(customer_params)
+        render json: customer, status: :ok
+    end
+
+    private 
+
+    def customer_params
+        params.permit(:name, :username, :email, :password)
+    end 
+
 end
