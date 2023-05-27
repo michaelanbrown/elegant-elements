b39d2f253f78cd743788b80ea28dad87ce0b87de
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-27 16:18:24 -0700

    index and show added to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,10 @@
 class OrdersController < ApplicationController
+
+    def index 
+        render json: Order.all, status: :ok
+    end
+
+    def show
+        render json: @order, status: :ok
+    end
 end
