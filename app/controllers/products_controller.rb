42fc1e605017a3c4befc82713470b073f88285cf
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-28 16:29:23 -0700

    product controller built our full CRUD

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,33 @@ class ProductsController < ApplicationController
     def index 
         render json: Product.all, status: :ok
     end
-    
+
     def show
         render json: @product, status: :ok
+    end
+
+    def create
+        product = Product.create!(product_params)
+        render json: product, status: :created
+    end
+
+    def update
+        @product.update!(update_product_params)
+        render json: @product, status: :accepted
+    end
+
+    def destroy
+        @product.destroy
+        head :no_content 
+    end 
+
+    private
+
+    def product_params
+        params.permit(:jewelry, :price, :quantity, :customization_id, :order_id)
+    end
+    
+    def update_product_params
+        params.permit(:jewelry, :price, :quantity)
     end
 end
