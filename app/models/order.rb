f59e5414ebb49bd2bc1ee09a7ea5e616eb26e723
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-30 19:36:18 -0700

    can only have one in progress order at a time

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,6 +9,7 @@ class Order < ApplicationRecord
     validates :shipping, presence: true, numericality: { equal_to: 7.00 }
     validates :status, presence: true
     validate :within_24_hours, on: :update
+    validate :in_progress
 
     private
 
@@ -17,6 +18,15 @@ class Order < ApplicationRecord
 
         if Time.at(created_at.to_i) < Time.at(Time.now-1.day.to_i)
             errors.add(:status, "must be within 24 hours of order")
+        end
+    end
+
+    def in_progress
+        orders = Order.where(customer_id: current_customer.id)
+        return if (status == "in progress" && orders.find_by(status: "in progress") == false)
+
+        if (status == "in progress" && orders.find_by(status: "in progress"))
+            errors.add(:status, "You already have an order in progress")
         end
     end
 end
