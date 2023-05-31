f3d14ec7f9ed1de65dab5633f5b7c5a17adfb7bd
Michaela Brown <m_brownbrown@yahoo.com>
2023-05-30 18:23:20 -0700

    custom validation for personalization cannot have spaces if word

--- app/models/customization.rb
+++ app/models/customization.rb
@@ -5,4 +5,16 @@ class Customization < ApplicationRecord
     validates :custom_type, presence: true, inclusion: { in: %w(phrase word date) }
     validates :price, numericality: { greater_than: 0 }
     validates :personalization, presence: true
+    # validates :personalization, format: { without: /\s/ }, scope: { :word_personalization }
+    validate :word_personalization
+
+    private
+    
+    def word_personalization
+        return if (custom_type == "word" && personalization.index(" ").to_i < 0)
+
+        if (custom_type == "word" && personalization.index(" "))
+            errors.add(:status, "Cannot be multiple words")
+        end
+    end
 end
