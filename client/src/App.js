0fb53d2d9b4ea9102a54faf086b2d5ad1aac0f74
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-03 16:34:53 -0700

    usercontext created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
+import { useContext } from 'react';
+import { UserContext } from './components/context/Customer';
 import Header from './components/Header';
 import Welcome from './components/Welcome';
 
 function App() {
+  // const { currentCustomer, setCurrentCustomer } = useContext(UserContext);
   return (
     <main>
       <Routes>
