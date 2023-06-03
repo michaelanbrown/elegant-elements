ec82c0a5e5048cfc795a986cca8cbf05ee4fdeeb
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-03 16:10:59 -0700

    address create

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,4 +3,15 @@ class AddressesController < ApplicationController
     def index 
         render json: Address.all, status: :ok
     end
+
+    def create
+        order = Address.create!(address_params)
+        render json: order, status: :created
+    end
+
+    private
+
+    def address_params
+        params.permit(:street, :unit, :city, :state, :zip).merge(customer_id: current_customer.id)
+    end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -4,11 +4,5 @@ class Address < ApplicationRecord
     validates :street, presence: true
     validates :city, presence: true
     validates :state, presence: true, length: { is: 2 }
-    validates :zip, presence: true
-    validate :zip_code_length_and_format
-
-    private
-    def zip_code_length_and_format
-        return if (zip.to_s.size == 5)
-    end
+    validates :zip, presence: true, length: { is: 5 }
 end
