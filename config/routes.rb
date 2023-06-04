b85850330785cbb0ede9b99a3a8f140cd9d7a69c
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-03 17:27:54 -0700

    added authorized_user, login, and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :orders
   resources :customizations
   resources :customers
+
+  post "/login", to: "sessions#create" 
+  delete "/logout", to: "sessions#destroy"
+  get "/authorized_user", to: "customers#show"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
