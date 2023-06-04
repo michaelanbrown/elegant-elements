3c14d9407ffd7f85f2f9e6cc646636c3e26a9c85
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-03 17:36:01 -0700

    added logout

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,22 @@
-import React from "react";
 import '../App.css'
-// import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
-// import { UserContext } from '../context/User';
+import React, { useContext } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { UserContext } from './context/User';
 
 export default function NavBar ()  {
-    // const { currentUser, setCurrentUser } = useContext(UserContext);
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+    const navigate = useNavigate();
+    const handleLogOut = () => {
+        fetch(`/logout`, {
+          method:"DELETE"
+        })
+        .then(res =>{
+          if(res.ok){
+            setCurrentUser(false)
+            navigate(`/`)
+          }
+        })
+      }
 
     return (
         <nav className="NavBar">
@@ -13,6 +24,8 @@ export default function NavBar ()  {
             <NavLink className="Navelements" to="/">Welcome</NavLink>
             <br/>
             <NavLink className="Navelements" to="/login">Login</NavLink>
+            <br/>
+            { currentUser ? <button className="logout" onClick={handleLogOut}>Logout</button> : null }
         </nav>
     )
 }
