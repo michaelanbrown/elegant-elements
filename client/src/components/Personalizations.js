fdaea9d617a90f26f55206b17e71751159479817
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-04 16:05:20 -0700

    customizations page

--- client/src/components/Personalizations.js
+++ client/src/components/Personalizations.js
@@ -6,8 +6,8 @@ function Personalizations({ cust }) {
     const customType = cust.custom_type.slice(0,1).toUpperCase() + cust.custom_type.slice(1,cust.custom_type.length)
 
     return (
-        <div>
-            {customType}
+        <div className='customization'>
+            {customType} - {cust.personalization}
         </div>
     )
 }
