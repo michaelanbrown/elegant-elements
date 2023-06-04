278bf1c58f905cd5c130140dafe213dd4ed1e613
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-03 19:48:27 -0700

    added intex to orders for address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_03_224352) do
+ActiveRecord::Schema.define(version: 2023_06_04_024612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2023_06_03_224352) do
     t.string "status", default: "in progress"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "address_id", null: false
+    t.index ["address_id"], name: "index_orders_on_address_id"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
@@ -68,6 +70,7 @@ ActiveRecord::Schema.define(version: 2023_06_03_224352) do
   end
 
   add_foreign_key "addresses", "customers"
+  add_foreign_key "orders", "addresses"
   add_foreign_key "orders", "customers"
   add_foreign_key "products", "customizations"
   add_foreign_key "products", "orders"
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,15 @@ c1 = Customization.create(custom_type: "phrase", price: 4.00, personalization: "
 c2 = Customization.create(custom_type: "word", price: 2.00, personalization: "Smiles")
 c3 = Customization.create(custom_type: "date", price: 2.00, personalization: "01.26.2021")
 
-o1 = Order.create(customer_id: u1.id, total: 20.00, discount: 0.00, shipping: 7.00, status: "completed")
-o2 = Order.create(customer_id: u2.id, total: 19.00, discount: 0.00, shipping: 7.00, status: "canceled")
-o3 = Order.create(customer_id: u3.id, total: 39.00, discount: 0.00, shipping: 7.00, status: "completed")
+a1 = Address.create(street: "1234 Main St", unit: nil, city: "Sacramento", state: "CA", zip: "93642", customer_id: u1.id)
+a2 = Address.create(street: "589 Real Dr", unit: "4A", city: "New York City", state: "NY", zip: "00512", customer_id: u2.id)
+a3 = Address.create(street: "654 Beach Cir", unit: nil, city: "Myrtle Beach", state: "SC", zip: "36542", customer_id: u3.id)
+
+o1 = Order.create(customer_id: u1.id, address_id: a1.id, total: 20.00, discount: 0.00, shipping: 7.00, status: "completed")
+o2 = Order.create(customer_id: u2.id, address_id: a2.id, total: 19.00, discount: 0.00, shipping: 7.00, status: "canceled")
+o3 = Order.create(customer_id: u3.id, address_id: a3.id, total: 39.00, discount: 0.00, shipping: 7.00, status: "completed")
 
 p1 = Product.create(jewelry: "necklace", price: 10.00, quantity: 1, customization_id: c3.id, order_id: o2.id)
 p2 = Product.create(jewelry: "bracelet", price: 9.00, quantity: 1, customization_id: c1.id, order_id: o1.id)
 p3 = Product.create(jewelry: "keychain", price: 8.00, quantity: 2, customization_id: c2.id, order_id: o3.id)
 p4 = Product.create(jewelry: "necklace", price: 10.00, quantity: 1, customization_id: c2.id, order_id: o3.id)
-
-a1 = Address.create(street: "1234 Main St", unit: nil, city: "Sacramento", state: "CA", zip: "93642", customer_id: u1.id)
-a2 = Address.create(street: "589 Real Dr", unit: "4A", city: "New York City", state: "NY", zip: "00512", customer_id: u2.id)
-a3 = Address.create(street: "654 Beach Cir", unit: nil, city: "Myrtle Beach", state: "SC", zip: "36542", customer_id: u3.id)
