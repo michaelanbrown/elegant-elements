53b88749d19b98c213a4e680e72fbc58d57c25d8
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-03 17:41:43 -0700

    built out signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useContext } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { UserContext } from './components/context/User';
 import Header from './components/Header';
 import Welcome from './components/Welcome';
 import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   const { currentUser, setCurrentUser } = useContext(UserContext);
+  const [customers, setCustomers] = useState([])
+  const [errors, setErrors] = useState([])
 
   useEffect(() => {
     fetch("/authorized_user")
@@ -19,7 +22,19 @@ function App() {
         });
       }
     })
+    getCustomers();
   },[])
+
+  function getCustomers() {
+    fetch("/customers")
+    .then((res) => {
+      if(res.ok){
+        res.json().then(setCustomers)
+      } else {
+        res.json().then(json => setErrors([json.error]))
+      }
+    })
+  }
 
 
   return (
@@ -27,6 +42,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Welcome/>} />
+        <Route path="/signup" element={<Signup customers={customers} setCustomers={setCustomers}/>} />
         <Route path="/login" element={<Login/>} />
       </Routes>
     </main>
