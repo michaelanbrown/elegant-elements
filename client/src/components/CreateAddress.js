c99de3084c941e90e8638196b0b0edd1782f7440
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-03 19:28:04 -0700

    create address css'

--- client/src/components/CreateAddress.js
+++ client/src/components/CreateAddress.js
@@ -5,10 +5,42 @@ import { UserContext } from './context/User';
 function CreateAddress(){
     const { currentCustomer, setCurrentCustomer } = useContext(UserContext);
     const [errors, setErrors] = useState([])
+    const [formData, setFormData] = useState({
+        street: "",
+        unit: "",
+        city: "",
+        state: "",
+        zip: "",
+    })
+    const {street, unit, city, state, zip} = formData
+
+    function handleChange(e) {
+        setFormData({
+            ...formData,
+            [e.target.name] : e.target.value
+        });
+    }
 
     return(
         <div>
-
+            New Address
+            <br/>
+            <br/>
+            <form>
+                Street: <input className='addressform' type="text" name="street" value={street} onChange={handleChange} />
+                <br/>
+                Unit: <input className='addressform' type="password" name="unit" value={unit} onChange={handleChange} />
+                <br/>
+                City: <input className='addressform' type="text" name="city" value={city} onChange={handleChange} />
+                <br/>
+                State: <input className='addressform' type="password" name="state" value={state} onChange={handleChange} />
+                <br/>
+                Zip: <input className='addressform' type="password" name="zip" value={zip} onChange={handleChange} />
+                <br/>
+                <input type="submit" value="Submit" />
+            </form>
+            <br/>
+            { errors ? errors.map(error => <div className='error' key={error}>{error}</div>) :null }
         </div>
     )
 }
