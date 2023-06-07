fb4124e5691a3931d7858a082b4113b1eea0c272
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-06 22:47:42 -0700

    customizatio ncaptured and custom_type uppercased

--- client/src/components/ProductsList.js
+++ client/src/components/ProductsList.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import '../App.css'
+import { UserContext } from './context/User';
 
 function ProductsList({ product, customizations }) {
+    const { currentCustomer, setCurrentCustomer } = useContext(UserContext);
+    const [custCustomization, setCustCustomization] = useState(false)
     const productType = product.jewelry.slice(0,1).toUpperCase() + product.jewelry.slice(1,product.jewelry.length)
-    console.log(customizations)
+    
+    useEffect(() => {
+        (customizations.map(cust => {
+            if (cust.id == product.customization_id) {
+                setCustCustomization(cust)
+                return cust
+            }
+        }))
+    }, [customizations])
+
+    const customType = custCustomization.custom_type ? custCustomization.custom_type.slice(0,1).toUpperCase() + custCustomization.custom_type.slice(1,custCustomization.custom_type.length) : null
 
     return (
         <div>
             {productType}
+            { custCustomization ? <p>Customization: {customType} </p> : null}
         </div>
     )
 }
