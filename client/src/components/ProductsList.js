07869735c36c368175ff0c8d92e735f8eca1a361
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-07 23:25:10 -0700

    capitalize custom type and jewelry

--- client/src/components/ProductsList.js
+++ client/src/components/ProductsList.js
@@ -14,7 +14,7 @@ function ProductsList({ product, customizations }) {
         }))
     }, [customizations])
 
-    const customType = custCustomization.custom_type ? custCustomization.custom_type.slice(0,1).toUpperCase() + custCustomization.custom_type.slice(1,custCustomization.custom_type.length) : null
+    const customType = custCustomization.custom_type ? custCustomization.custom_type : null
     const customizationLine = custCustomization ? <p>Customization: {customType} - "{ custCustomization.personalization }" </p> : null
 
     return (
