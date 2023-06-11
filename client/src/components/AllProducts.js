37dd4c2649ad03be75d87194d847d54d58142636
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-10 20:15:20 -0700

    css for all products available

--- client/src/components/AllProducts.js
+++ client/src/components/AllProducts.js
@@ -5,8 +5,10 @@ function AllProducts() {
     
 
     return (
-        <div>
-            
+        <div className="productcontainer">
+            <img className="productimg" src={"https://cdn.shopify.com/s/files/1/0008/8932/3571/products/DSC_0019_800x800_1200x1200.jpg?v=1541563917"} alt="Handstamped Necklace Photo" width="30%" height="30%"/>
+            <br/>
+            <p className="productform">Custom Handstamped Necklace</p>
         </div>
     )
 }
