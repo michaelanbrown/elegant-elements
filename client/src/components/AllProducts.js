135f54fc7f12f9b789c3aae1dd782f6f0b8c3dbf
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-10 20:46:31 -0700

    use state to view order form

--- client/src/components/AllProducts.js
+++ client/src/components/AllProducts.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css'
 
 function AllProducts({ product }) {
     const productName = product.name.slice(0,1).toUpperCase() + product.name.slice(1, product.name.length)
+    const [viewOrderForm, setViewOrderForm] = useState(false)
 
     return (
-        <div className="productcontainer">
-            <img className="productimg" src={product.img} alt={product.name} width="30%" height="30%"/>
-            <br/>
-            <p className="productform">Custom Handstamped {productName}</p>
+        <div>
+            <div className="productcontainer">
+                <img className="productimg" src={product.img} alt={product.name} width="30%" height="30%"/>
+                <br/>
+                <p className="productform">Custom Handstamped {productName}
+                <br/>
+                <button>Add to Order</button></p>
+            </div>
         </div>
     )
 }
