54bf1be8686ccc3cc868c9294f6c3dc0fd6fb529
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-10 21:49:50 -0700

    added in_progress_product_count to customer

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,14 +1,20 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :username, :password_digest, :products
+  attributes :id, :name, :email, :username, :password_digest, :products, :in_progress_product_count
 
   has_many :orders
   has_many :addresses
 
   def products
-    @order_ids = object.orders.map{ |o| o.id}
+    @order_ids = object.orders.map{ |o| o.id }
     @product_arrays = @order_ids.map { |o| Product.where(order_id: o)}
     @prods = []
     @product_arrays.map{|p| p.map{|p| @prods.push(p)}}
     @prods.each{|p| p.jewelry = p.jewelry.capitalize}.uniq{|p| p.jewelry && p.customization_id}
+  end
+
+  def in_progress_product_count
+    @in_progess_order = Order.where(status: "in progress", customer_id: object.id)
+    @in_progess_order.map{ |o| o.id }
+    Product.where(order_id: @in_progess_order).count
   end
 end
