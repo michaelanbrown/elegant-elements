47894c275e114119716010347be6f06e3525d69e
Michaela Brown <m_brownbrown@yahoo.com>
2023-06-11 18:17:40 -0700

    added editable attribute to orders

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :customer_id, :total, :discount, :shipping, :status
+  attributes :id, :customer_id, :total, :discount, :shipping, :status, :editable
 
   has_many :products
   has_many :customizations
@@ -11,6 +11,16 @@ class OrderSerializer < ActiveModel::Serializer
     @product_total = object.products.map { |p| p.price * p.quantity }.sum
     @customization_total = object.products.map { |p| p.quantity * p.customization.price }.sum
     @product_total + @customization_total + object.shipping
+  end
+
+  def editable
+    if object.status == "in progress"
+      return true
+    elsif object.status == "pending" && Time.at(created_at.to_i) > Time.at(Time.now-1.day.to_i)
+      return true
+    else
+      return false
+    end
   end
 
 end
